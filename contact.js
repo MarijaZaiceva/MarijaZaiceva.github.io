aaa92f5cc96d6da792675be658d457e9e2d79acd
MarijaZaiceva <3aicevamarija@gmail.com>
2024-11-03 23:20:23 +0200

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,29 +1,18 @@
-document.getElementById("contactForm").onsubmit = function(event) {
-    event.preventDefault();
+    function sendMail(){
+        let formData ={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    }
 
-    // Collecting the form data
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-
-    //Email validation
     const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!email.match(emailPattern)) {
         document.getElementById("responseMessage").innerText = "Please enter a valid email address.";
         return; 
     }
-
     
     // If email is valid, prepare to send the data
-    const formData = {
-        name: name,
-        email: email,
-        message: message
-    };
-
-
-    emailjs.send("service_j43ay6v", "template_6xje15m", formData)
+    emailjs.send("service_j43ay6v", "template_6xje15m", formData).then(alert("Email Sent"))
         .then((response) => {
             document.getElementById("responseMessage").innerText = "Message sent successfully!";
         }, (error) => {
