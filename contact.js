9cc05b52cc61c66c64ddb5a26d7934278266f3b6
MarijaZaiceva <3aicevamarija@gmail.com>
2024-11-06 21:27:19 +0200

    final touches

--- contact.js
+++ contact.js
@@ -6,28 +6,28 @@
     }
     
     if (document.getElementById("g-recaptcha-response").value==""){
-        document.getElementById("responseMessage").innerText = "Captcha not solved";
+        document.getElementById("responseMessage").innerText = "Kaptcha nav derīga";
         return;
     }
 
     const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!formData.email.match(emailPattern)) {
-        document.getElementById("responseMessage").innerText = "Please enter a valid email address.";
+        document.getElementById("responseMessage").innerText = "Lūdzu, ievadiet derīgu e-pastu.";
         return; 
     }
     
     // If email is valid, prepare to send the data
     emailjs.send("service_j43ay6v","template_6xje15m", formData).then(alert("Email Sent"))
         .then((response) => {
-            document.getElementById("responseMessage").innerText = "Message sent successfully!";
+            document.getElementById("responseMessage").innerText = "Jūsu ziņojums ir veiksmīgi nosūtīts!";
             console.log('SUCCESS!', response.status, response.text);
         }, (error) => {
-            document.getElementById("responseMessage").innerText = "Error sending message: " + error.text;
+            document.getElementById("responseMessage").innerText = "Kļūda: " + error.text;
             console.log('FAILED...', error);
         });
     
     // For demonstration purposes, we just log the data
     console.log(formData);
-    document.getElementById("responseMessage").innerText = "Message would be sent!";
+    document.getElementById("responseMessage").innerText = "Ziņojums ir gatavs nosūtīšanai";
     document.getElementById("contactForm").reset();
 }
